276d4fa9d5a7ecb9417cb93fa73a9dc8381fbf03
RaymondOchoa <ochoaraymondb@yahoo.com>
2022-12-04 18:16:23 -0800

    completed assessment

--- code-challenges.js
+++ code-challenges.js
@@ -11,6 +11,7 @@
 // Set one:
 const fruit1 = "apple"
 const fruit2 = "banana"
+
 // Expected outcome: "banana"
 
 // Set two:
@@ -21,48 +22,84 @@ const fruit4 = "kiwi"
 // --------------------1) Create a function that takes in a number and determines if the given number is below boiling point, at boiling point, or above boiling point. Boiling point is 212 degrees Fahrenheit. Use the test variables provided below.
 
 // Pseudo code:
+// create a function that determines the numbers relativatity to boiling point
+// if the temp is above 212 it will return "above boiling temp"
+// if the temp is below 212 it will return "below boiling temp"
+// if the temp is at 212 it will return "at boiling temp"
 
-const temp1 = 42
-// Expected output: "42 is below boiling point"
+// const tempCheck = (temp) => {
+//     if (temp > 212)
+//     return `${temp} is above boiling temperature`  
+//     else if (temp < 212)
+//     return `${temp} is below boiling temperature`   
+//     else if (temp === 212)
+//     return `${temp} is at boiling temperature`    
+ //}
+ //console.log(tempCheck(42))
+// // Expected output: "42 is below boiling point"
 
-const temp2 = 350
-// Expected output: "350 is above boiling point"
+// const temp2 = 350
+//console.log(tempCheck(350))
+// // Expected output: "350 is above boiling point"
 
-const temp3 = 212
-// Expected output: "212 is at boiling point"
+//const temp3 = 212
+ //console.log(tempCheck(212))
+// // Expected output: "212 is at boiling point"
+// if 
 
-// --------------------2) Create the code that will combine the two arrays and return the length using the test variables provided below.
+// // --------------------2) Create the code that will combine the two arrays and return the length using the test variables provided below.
 
-// Pseudo code:
+// // Pseudo code:
+// create a code that combines the arrays and returns the total index length
+// when combined it should log 9 total 
 
-const padres1984WorldSeriesRuns = [2, 5, 2, 2, 4]
-const padres1998WorldSeriesRuns = [6, 3, 5, 3]
-// Expected output: 9
+// const padres1984WorldSeriesRuns = [2, 5, 2, 2, 4]
+// const padres1998WorldSeriesRuns = [6, 3, 5, 3]
+// const totalRuns = (padres1984WorldSeriesRuns.concat(padres1998WorldSeriesRuns))
+// console.log(totalRuns.length)
 
-// --------------------3) Create the code that will reverse the letters of a string using the test variable provided below.
+// // Expected output: 9
 
-// Pseudo code:
+// // --------------------3) Create the code that will reverse the letters of a string using the test variable provided below.
+
+// // Pseudo code:
+// create a code that will reverse the letters in the string
+// when ran it should log "2202 letoH"
 
 const currentCohort = "Hotel 2022"
-// Expected output: "2202 letoH"
+var splitCohort = (currentCohort.split(""))
+var reveresedCohort = (splitCohort.reverse())
+console.log(reveresedCohort)
+// // Expected output: "2202 letoH"
 
-// --------------------4) Create the code that will return the last index of the given value from the array using the test variables provided below.
+// // --------------------4) Create the code that will return the last index of the given value from the array using the test variables provided below.
 
-// Pseudo code:
+// // Pseudo code:
+// create a code that will return the last index of the given value 
+// when ran it should log 7 for the 1st given value
+// when ran it should log 8 for the 2nd given value
 
-const myNumbers = [13, 34, 42, 5, 5, 10, 27, 42, 10]
-const givenValue1 = 42
-// Expected output: 7
+// const myNumbers = [13, 34, 42, 5, 5, 10, 27, 42, 10]
+// const givenValue1 = 42
+// console.log(myNumbers.lastIndexOf(givenValue1))
+// // // Expected output: 7
 
-const givenValue2 = 10
-// Expected output: 8
+// const givenValue2 = 10
+// console.log(myNumbers.lastIndexOf(givenValue2))
+// // // Expected output: 8
 
-// --------------------5) Create the code that will sort all the numbers in an array from largest to smallest. Use the test variables provided below.
+// // --------------------5) Create the code that will sort all the numbers in an array from largest to smallest. Use the test variables provided below.
 
-// Pseudo code:
+// // Pseudo code:
+// create a code that will sort all the number from largest to smallest
+// using a descending sorting method create a renamed sorted set of temperatures
+//when ran it should log the numbers in order from greatest to smallest
+// const sanDiegoSummerTemperatures = [79, 80, 72, 73, 82, 77, 76]
+// // // Expected output: [82, 80, 79, 77, 76, 73, 72]
+// const sortedSummer = (sanDiegoSummerTemperatures.sort((a,b)=>b-a))
 
-const sanDiegoSummerTemperatures = [79, 80, 72, 73, 82, 77, 76]
-// Expected output: [82, 80, 79, 77, 76, 73, 72]
-
-const sanDiegoWinterTemperatures = [59, 68, 62, 59, 66, 67, 66]
-// Expected output: [68, 67, 66, 66, 62, 59, 59]
+// console.log(sortedSummer)
+// const sanDiegoWinterTemperatures = [59, 68, 62, 59, 66, 67, 66]
+// const sortedWinter = (sanDiegoWinterTemperatures.sort((a,b)=>b-a))
+// // // Expected output: [68, 67, 66, 66, 62, 59, 59]
+// console.log(sortedWinter)
--- coding-concepts.js
+++ coding-concepts.js
@@ -9,46 +9,46 @@
 const colors = ["tangerine", "magenta", "lilac", "daffodil"]
 // console.log(colors.push("indigo"))
 
-// a) Your answer:
+// a) Your answer: tangerine, magenta, lilac, daffodil, indigo
 // b) Verify and explain:
 
 // --------------------1) What will this log?
 
 const cohort = "LEARN 2022"
-// console.log(cohort.length)
+ //console.log(cohort.length)
 
-// a) Your answer:
-// b) Verify and explain:
+// a) Your answer: 10
+// b) Verify and explain: this logged 10 because the indexes count spaces as characters 
 
 // --------------------2) What will this log?
 
 const greeting = "Hello World!"
-// console.log(greeting[4])
+//console.log(greeting[4])
 
-// a) Your answer:
-// b) Verify and explain:
+// a) Your answer: o
+// b) Verify and explain: this logged "o" because the count begins at 0 and "o" is in the 4th space
 
 // --------------------3) What will this log?
 
 const languages = ["JavaScript", "Ruby", "Python", "C++"]
 const index = 1
-// console.log(languages[index])
+//console.log(languages[index])
 
-// a) Your answer:
-// b) Verify and explain:
+// a) Your answer: Ruby
+// b) Verify and explain: this logged Ruby because the language thats being referred to in the console.log is in the 1 position, that ends up being ruby because the count began at 0
 
 // --------------------4) What will this log?
 
 const weekendDays = ["saturday", "sunday"]
-// console.log(weekendDays.toUpperCase())
+//console.log(`${weekendDays}`.toUpperCase())
 
-// a) Your answer:
-// b) Verify and explain:
+// a) Your answer: SATURDAY SUNDAY
+// b) Verify and explain: initially this logged an error but after adding string interpolation around weekendDays in the console.log line (not sure if i was supposed to do that lol)
 
 // --------------------5) What will this log?
 
 const dataTypes = ["number", "string", "Boolean", "undefined"]
-// console.log(typeof dataTypes.length)
+console.log(typeof dataTypes.length)
 
-// a) Your answer:
-// b) Verify and explain:
+// a) Your answer: number
+// b) Verify and explain: this logged "number" because it ran the typeof data first and didn't end up checking the .length
